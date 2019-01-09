efc31ce6fcb38fd42add2b7c855ed97d2ba8c4fd
Almog <almoggueta1@gmail.com>
2019-01-09 12:54:15 -0800

    state equal

--- main.cpp
+++ main.cpp
@@ -17,11 +17,15 @@ int main() {
     State<string> *st = new State<string>("almog");
     State<string> *st2 = new State<string>("chagit");
     State<string> *st3 = new State<string>("3");
+    State<string> * st4 = new State<string>("3");
     st->setFrom(st2);
     st2->setFrom(st3);
     State<string> *fromState = st->getFrom();
     State<string> *from2 = st->getFrom()->getFrom();
 
+    if(st4->equals(st3) == true){
+        cout<<"equal"<<endl;
+    }
     cout << st->getState() << endl;
     cout << fromState->getState() << endl;
     cout << from2->getState() << endl; //need to be 3
