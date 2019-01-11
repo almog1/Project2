f36dd553614295a6a8351618502998d13ddadf6b
Almog <almoggueta1@gmail.com>
2019-01-11 06:19:13 -0800

    bfs search first update 11.1

---
+++ BFS.h
@@ -0,0 +1,53 @@
+//
+// Created by almogg on 1/11/19.
+//
+
+#ifndef PROJECT2_BFS_H
+#define PROJECT2_BFS_H
+
+#include "Searcher.h"
+#include <list>
+#include <queue>
+
+using std::list;
+
+template<class T>
+class BFS : public Searcher<T> {
+
+public:
+    string search(ISearchable<T> *Isearchable) override {
+        list<State<T> *> open;
+        list<State<T> *> close;
+        State<T> *state;
+
+        string path;
+
+        open.push_back(Isearchable->getInitializeState());
+
+        typename list<State<T> *>::iterator it;
+
+        //while the queue isn't empty
+        while (!open.empty()) {
+            state = open.front(); //take the first one
+            close.push_back(state); //to add to path been
+            open.pop_front();
+
+            //check if its the goal
+            if (state->equals(Isearchable->getGoalState())) {
+                return "path";
+            }
+            list<State<T> *> next;
+            next = Isearchable->getAllPossibleStates(state);
+            for (it = next.begin(); it != next.end(); it++) {
+                //if not in closed list
+                if (find(close.begin(), close.end(), (*it)) == close.end()) {
+                    //its not in the closed list
+                    //   close.push_back(*it);
+                    open.push_back(*it);
+                }
+            }
+        }
+    }
+};
+
+#endif //PROJECT2_BFS_H
